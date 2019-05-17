640586e52ae9b211e5ea7b5c856ef84fd15895ee
Raul Gherasim <rawul>
2019-05-18 02:13:31 +0300

    login modalf..asd.as.d.a

--- portal/src/app/dashboard/dashboard.module.ts
+++ portal/src/app/dashboard/dashboard.module.ts
@@ -5,12 +5,24 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavBarComponent } from './dashboard/nav-bar/nav-bar.component';
 import { InteractiveMapComponent } from './dashboard/interactive-map/interactive-map.component';
-
+import { MatDialogModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { LoginModalComponent } from './dashboard/login-modal/login-modal.component'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './dashboard/login.service';
 @NgModule({
-  declarations: [DashboardComponent, NavBarComponent, InteractiveMapComponent],
+  declarations: [DashboardComponent, NavBarComponent, InteractiveMapComponent, LoginModalComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    MatDialogModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule
+  ],
+  entryComponents: [LoginModalComponent],
+  providers:[LoginService]
 })
 export class DashboardModule { }
---
+++ portal/src/app/dashboard/dashboard/login-modal/login-modal.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss']
+})
+export class LoginModalComponent implements OnInit {
+  user = {
+    email: '',
+    password: ''
+  }
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    this.loginService.login(this.user.email, this.user.password).subscribe(x => {
+      console.log(x);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+}
