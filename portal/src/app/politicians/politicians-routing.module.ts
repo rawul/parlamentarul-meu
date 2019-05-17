7e9879af507d21a1745ecb525a951e57771db293
alexzagrean <alexandru.zagrean9@gmail.com>
2019-05-18 01:05:54 +0300

    Personal profile

--- portal/src/app/politicians/politicians-routing.module.ts
+++ portal/src/app/politicians/politicians-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PoliticiansComponent } from './politicians/politicians.component';
+import { PersonalProfileComponent } from './personal-profile/personal-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PoliticiansComponent
+  },
+  {
+    path: 'profile',
+    component: PersonalProfileComponent
   }
 ];
 
