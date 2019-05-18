cdcc50b75cd2b3ad47479a0b718e4a40db98dabb
Raul Gherasim <rawul>
2019-05-18 16:37:15 +0300

    changessss

--- portal/src/app/politicians/politicians.module.ts
+++ portal/src/app/politicians/politicians.module.ts
@@ -8,6 +8,10 @@ import { PoliticiansRoutingModule } from './politicians-routing.module';
 import { PoliticiansComponent } from './politicians/politicians.component';
 import { PersonalProfileComponent } from './personal-profile/personal-profile.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MatDialogModule, MatInputModule, MatButtonModule, MatMenuModule, MatCheckboxModule, MatTooltipModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TextFieldModule } from '@angular/cdk/text-field';
 
 @NgModule({
   declarations: [PoliticiansComponent, PersonalProfileComponent, NavBarComponent],
@@ -15,7 +19,19 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     CommonModule,
     PoliticiansRoutingModule,
     MatTabsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatDialogModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    TextFieldModule,
+    FontAwesomeModule,
+    MatTooltipModule
   ]
 })
 export class PoliticiansModule { }
