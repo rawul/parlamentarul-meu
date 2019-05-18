7bd7fbde13bf98d74788f4c56708363337d2a7c6
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-18 04:12:03 +0300

    get all politicians route

--- backend/controllers/PoliticianController.js
+++ backend/controllers/PoliticianController.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const politicanService = require("../services/PoliticianService");
 
-router.get("/politician/:county", politicanService.getPoliticians);
+router.get("/politician", politicanService.getPoliticians);
+router.get("/politician/:county", politicanService.getPoliticiansByCounty);
 
 module.exports = router;
--- backend/services/PoliticianService.js
+++ backend/services/PoliticianService.js
@@ -5,6 +5,15 @@ let Senator = require('../models/SenatorModel');
 
 const PoliticianService = {
   getPoliticians: async (req, res) => {
+    try {
+      const deputies = await Deputy.find({}).lean().exec();
+      const senators = await Senator.find({}).lean().exec();
+      res.json([...deputies, ...senators]);
+    } catch (err) {
+      res.status(400).json({ message: 'There has been an error' })
+    }
+  },
+  getPoliticiansByCounty: async (req, res) => {
     const county = req.params.county;
     try {
       const deputies = await Deputy.find({ county }).lean().exec();
