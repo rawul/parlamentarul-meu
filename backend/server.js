d69a0ebb31861d1cb960cbed806a5c625831e5c2
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-18 17:37:24 +0300

    link user with a politician obj

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const mongoose = require('mongoose');
 const config = require('./DB.js');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+
+mongoose.connect(config.DB, { useNewUrlParser: true, useCreateIndex: true, }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database' + err) }
 );
--- backend/services/UserService.js
+++ backend/services/UserService.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 const uuidv4 = require('uuidv4')
 const User = require("../models/UserModel");
+
+const politicianService = require('./PoliticianService');
+
 const passport = require('passport');
 
 const UserService = {
   loginUser: async (req, res) => {
     const auth = await User.authenticate()(req.body.email, req.body.password);
     if (auth.user) {
-      auth.user.token = uuidv4();
-      await auth.user.save();
-      res.send({ token: auth.user.token })
+      const token = uuidv4();
+      await auth.user.update({ token })
+      const politician = await politicianService.getPoliticianByUser(auth.user)
+      res.send({ token, politician })
     } else {
       res.status(403).json({ message: 'Wrong username or password' })
     }
