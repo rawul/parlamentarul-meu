3d8afec08e6de5e6109ad309d9a5f65ee79b8e3f
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-18 23:22:25 +0300

    fix chat room token

--- backend/services/ChatService.js
+++ backend/services/ChatService.js
@@ -6,8 +6,6 @@ const uuidv4 = require('uuidv4');
 const Message = require('../models/MessageModel');
 const Chat = require('../models/ChatModel');
 
-let baseUrl = "localhost:4000/api/v1/chat/"
-
 const ChatService = {
   sendMessage: async (req, res) => {
     Chat.find(function (err, chat) {
@@ -15,13 +13,10 @@ const ChatService = {
         console.log(err);
       }
       else {
-        console.log(chat);
         if (chat == "[]")
           res.status(200).json();
         else {
-          baseUrl += req.params.token;
-          console.log({ baseUrl, p: req.params })
-          let message = new Message({ from: req.body.from, content: req.body.content, chatURL: baseUrl, timestamp: new Date().toISOString() });
+          let message = new Message({ from: req.body.from, content: req.body.content, chatURL: req.params.token, timestamp: new Date().toISOString() });
           message.save()
             .then(() => {
               res.status(200).json();
