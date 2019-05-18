10383783b0367d3c8455dc3ccce5b2400979e5c0
Denis-Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2019-05-18 19:51:09 +0300

    Add endpoint for chats retrieving

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -3,4 +3,6 @@ const router = require("express").Router();
 const messageService = require("../services/MessageService");
 
 router.post("/message", messageService.sendMessage);
+router.get("/chat/politician", messageService.getMessages);
+
 module.exports = router;
--- backend/services/MessageService.js
+++ backend/services/MessageService.js
@@ -38,7 +38,7 @@ const MessageService = {
       text: token
     }).then( async e => {
       baseUrl += token;
-      let chat = new Chat({url:baseUrl, subject: req.body.subject, politicianMail: req.body.to, userToken: token});
+      let chat = new Chat({url:baseUrl, subject: req.body.subject, politicianMail: req.body.to, userToken: token, messages: [req.body.content]});
       let message = new Message({from: req.body.from, content: req.body.content, chatURL: baseUrl, timestamp: new Date().toISOString()});
       await chat.save();
       await message.save();
@@ -46,6 +46,24 @@ const MessageService = {
     }).catch(
       err => console.log(err)
     );
+  },
+  getMessages: async(req, res) => {
+    const politicianEmail = req.query.email;
+    try {
+      const chats = await Chat.find({politicianMail: politicianEmail}).lean().exec();
+      for(var i = 0; i < chats.length; i++){
+        try {
+          const messages = await Message.find({chatURL: chats[i].url}).lean().exec();
+          chats[i].messages = messages;
+        } catch(err)  {
+          res.status(400).json({message: "Message could not be retrieved"});
+        }
+      }
+      res.status(200).json(chats);
+    } catch(err){
+      console.log(err);
+      res.status(400).json({ message: 'Error retrieving chats' });
+    }
   }
 }
 
