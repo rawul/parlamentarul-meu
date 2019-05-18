2c4852cf932e4a9feba0b07d16c8466a7d3fd0ff
Raul Gherasim <rawul>
2019-05-18 14:39:23 +0300

    changes

--- portal/src/app/dashboard/dashboard.module.ts
+++ portal/src/app/dashboard/dashboard.module.ts
@@ -7,7 +7,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavBarComponent } from './dashboard/nav-bar/nav-bar.component';
 import { InteractiveMapComponent } from './dashboard/interactive-map/interactive-map.component';
-import { MatDialogModule, MatInputModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatDialogModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatTooltipModule } from '@angular/material';
 import { LoginModalComponent } from './dashboard/login-modal/login-modal.component'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -30,7 +30,8 @@ import { TextFieldModule } from '@angular/cdk/text-field';
     MatMenuModule,
     MatCheckboxModule,
     TextFieldModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatTooltipModule
   ],
   entryComponents: [LoginModalComponent, ProfileModalComponent],
   providers: [LoginService, DashboardService]
--- portal/src/app/dashboard/dashboard/dashboard.component.ts
+++ portal/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ProfileModalComponent } from './profile-modal/profile-modal.component';
-import { Politician } from 'src/app/models/politician.model';
+import { Politician, PoliticianType } from 'src/app/models/politician.model';
 import { DashboardService } from '../dashboard.service';
 
 @Component({
@@ -11,7 +11,9 @@ import { DashboardService } from '../dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   judet = '';
-  politicians: Politician[] = [];
+  politicians: Politician[] = [
+    new Politician('bob', 'asd', 'usr', 'asda@asd.com', 'bogdanestilor nr 1230', 'omg', 'timis', PoliticianType.deputy)
+  ];
 
   constructor(public dialog: MatDialog, private dashService: DashboardService) { }
 
@@ -39,8 +41,13 @@ export class DashboardComponent implements OnInit {
     }
 
   }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(ProfileModalComponent);
+  openDialog(politician: Politician, showMsg: boolean): void {
+    const dialogRef = this.dialog.open(ProfileModalComponent, {
+      data: {
+        politician: politician,
+        showMsg: showMsg
+      }
+    });
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
