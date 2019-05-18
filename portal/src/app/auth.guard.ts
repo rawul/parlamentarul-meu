0778e2900160c4cd0c7efca3d0598bc83afa39f9
Raul Gherasim <rawul>
2019-05-18 21:41:21 +0300

    sopdgjpowemgopsadngopsd;

---
+++ portal/src/app/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivateChild, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivateChild {
+
+  constructor(private router: Router) { }
+
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (localStorage.getItem('token')) {
+      return true;
+    }
+    this.router.navigate(['']);
+    return false;
+
+  }
+
+}
