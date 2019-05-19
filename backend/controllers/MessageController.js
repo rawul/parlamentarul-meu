12a8c783706a1d73a8ea215f414c38e2f5cf1e8e
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-19 08:20:31 +0300

    fix fucking auth middleware for the fucking 9999 time

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-
+const authenticationMiddleware = require('../middleware/AuthenticationMiddleware');
 const messageService = require("../services/MessageService");
 
 router.post("/message", messageService.sendMessage);
-router.get("/chat/politician", messageService.getMessages);
 router.get("/chat/politician", authenticationMiddleware, messageService.getMessages);
 module.exports = router;
