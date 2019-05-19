463c86c9899759d6b5ef3bab1bd1c23d3b5b150e
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-19 08:06:21 +0300

    remove authentication from non authenticated user message send

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const messageService = require("../services/MessageService");
 const authenticationMiddleware = require('../middleware/AuthenticationMiddleware');
 
-router.post("/message", authenticationMiddleware, messageService.sendMessage);
+router.post("/message", messageService.sendMessage);
 router.get("/chat/politician", authenticationMiddleware, messageService.getMessages);
 module.exports = router;
