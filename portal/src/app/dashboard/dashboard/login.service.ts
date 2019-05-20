72ab71ead469e962e19db24d146bdf4e4e9aea91
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-20 15:23:28 +0300

    change api route

--- portal/src/app/dashboard/dashboard/login.service.ts
+++ portal/src/app/dashboard/dashboard/login.service.ts
@@ -11,6 +11,6 @@ export class LoginService {
     private http: HttpClient
   ) { }
   getLogin(username, password): Observable<any> {
-    return this.http.post('http://pm.dariuscostolas.me:2500/api/v1/login', { email: username, password: password });
+    return this.http.post('http://api.pm.dariuscostolas.me/api/v1/login', { email: username, password: password });
   }
 }
