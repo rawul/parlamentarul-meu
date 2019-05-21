c0ba22729953e9de9134040813479e3129732414
Darius Costolas <dariuscostolas@yahoo.com>
2019-05-21 11:53:07 +0300

    change mock data to be more proffiecient

--- backend/migrate.js
+++ backend/migrate.js
@@ -13,7 +13,7 @@ const generator = require('generate-password');
 
     Deputy.collection.insertMany(require('../scarping/people_info/dump/deputies'));
 
-    const testUser = await Deputy.collection.insertOne({ "name": "Emilut", "party": "PNL", "email": "raulgherasim@gmail.com", "address": "Alba Iulia, Str. Basarabiei nr. 3, jud. Alba", "pictureUrl": "http://www.landia.ro/wp-content/uploads/2018/01/emilut.jpg", "politicianType": "deputy", "county": "alba" });
+    const testUser = await Deputy.collection.insertOne({ "name": "Deputat Test", "party": "PNL", "email": "raulgherasim@gmail.com", "address": "Alba Iulia, Str. Basarabiei nr. 3, jud. Alba", "pictureUrl": "https://balance-global.com/wp-content/uploads/2015/10/test-user-avatar.png", "politicianType": "deputy", "county": "alba" });
 
     [... await Deputy.find({}).lean().exec(), ... await Senator.find({}).lean().exec()]
         .filter(politician => politician.email)
